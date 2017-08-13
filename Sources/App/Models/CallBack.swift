367c4ff806ed57ca5fded9a2428aa20db73ac8e2
Joe Jiang <happiness9721@gmail.com>
2017-08-14 00:55:22 +0800

    因為heroku沒辦法支援NSRegularExpression，所以先拿掉

--- Sources/App/Models/CallBack.swift
+++ Sources/App/Models/CallBack.swift
@@ -53,42 +53,38 @@ class CallBack {
   }
   
   func queryTRARoute() throws {
-    let regex = try NSRegularExpression(pattern: "^[\\u4e00-\\u9fa5]{2,4}\\s[\\u4e00-\\u9fa5]{2,4}$")
-    let nsString = message as NSString
-    if regex.matches(in: message, range: NSRange(location: 0, length: nsString.length)).count > 0 {
-      let messageArray = message.components(separatedBy: " ")
-      let fromStation = try TRAStation.makeQuery().filter("name", messageArray[0]).first()
-      let toStation = try TRAStation.makeQuery().filter("name", messageArray[1]).first()
-      if let fromStation = fromStation, let toStation = toStation {
-        let now = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.timeZone = TimeZone(secondsFromGMT: 3600 * 8)
-        dateFormatter.dateFormat = "yyyy/MM/dd"
-        let dateString = dateFormatter.string(from: now)
-        dateFormatter.dateFormat = "HHmm"
-        let fromTime = dateFormatter.string(from: now)
-        let toTime = dateFormatter.string(from: now.addingTimeInterval(3600 * 7))
-        
-        var url = "http://www.madashit.com/api/get-Tw-Railway?date=" + dateString
-        url += "&fromstation=" + (fromStation.id?.string ?? "")
-        url += "&tostation=" + (toStation.id?.string ?? "")
-        url += "&fromtime=" + fromTime + "&totime=" + toTime
-        
-        let json = try JSON(bytes: Data(contentsOf: URL(string: url)!).makeBytes())
-        if let railwaies = json.array {
-          var railwayInfo = String()
-          for railway in railwaies {
-            if railwayInfo.characters.count > 0 {
-              railwayInfo += "\n"
-            }
-            railwayInfo += railway.object?["車種"]?.string ?? ""
-            railwayInfo += " " + (railway.object?["車次"]?.int ?? 0).description
-            railwayInfo += " " + (railway.object?["經由"]?.string ?? "")
-            railwayInfo += " " + (railway.object?["開車時間"]?.string ?? "")
-            railwayInfo += "-" + (railway.object?["到達時間"]?.string ?? "")
+    let messageArray = message.components(separatedBy: " ")
+    let fromStation = try TRAStation.makeQuery().filter("name", messageArray[0]).first()
+    let toStation = try TRAStation.makeQuery().filter("name", messageArray[1]).first()
+    if let fromStation = fromStation, let toStation = toStation {
+      let now = Date()
+      let dateFormatter = DateFormatter()
+      dateFormatter.timeZone = TimeZone(secondsFromGMT: 3600 * 8)
+      dateFormatter.dateFormat = "yyyy/MM/dd"
+      let dateString = dateFormatter.string(from: now)
+      dateFormatter.dateFormat = "HHmm"
+      let fromTime = dateFormatter.string(from: now)
+      let toTime = dateFormatter.string(from: now.addingTimeInterval(3600 * 7))
+      
+      var url = "http://www.madashit.com/api/get-Tw-Railway?date=" + dateString
+      url += "&fromstation=" + (fromStation.id?.string ?? "")
+      url += "&tostation=" + (toStation.id?.string ?? "")
+      url += "&fromtime=" + fromTime + "&totime=" + toTime
+      
+      let json = try JSON(bytes: Data(contentsOf: URL(string: url)!).makeBytes())
+      if let railwaies = json.array {
+        var railwayInfo = String()
+        for railway in railwaies {
+          if railwayInfo.characters.count > 0 {
+            railwayInfo += "\n"
           }
-          replyMessage.add(message: railwayInfo.characters.count == 0 ? "七小時內尚無班次" : railwayInfo)
+          railwayInfo += railway.object?["車種"]?.string ?? ""
+          railwayInfo += " " + (railway.object?["車次"]?.int ?? 0).description
+          railwayInfo += " " + (railway.object?["經由"]?.string ?? "")
+          railwayInfo += " " + (railway.object?["開車時間"]?.string ?? "")
+          railwayInfo += "-" + (railway.object?["到達時間"]?.string ?? "")
         }
+        replyMessage.add(message: railwayInfo.characters.count == 0 ? "七小時內尚無班次" : railwayInfo)
       }
     }
   }
